71c1eaa2d0139ac4f08ef2e3d93195d112975a07
Deep <deepmehta.it@live.com>
2014-09-10 04:07:41 -0700

    Working Scala Hello World Application

---
+++ src/main/scala/hello/HelloWebApp.scala
@@ -0,0 +1,21 @@
+package hello
+
+import org.springframework.boot._
+import org.springframework.boot.autoconfigure._
+import org.springframework.stereotype._
+import org.springframework.web.bind.annotation._
+import HelloWebApp._
+
+object HelloWebApp {
+  def main(args: Array[String]) {
+    SpringApplication.run(classOf[DeepMe])
+    }
+}
+@Controller
+@EnableAutoConfiguration
+class DeepMe {
+  @RequestMapping(Array("/Deep"))
+  @ResponseBody
+  def home(): String = "Hello World!"
+ 
+}
